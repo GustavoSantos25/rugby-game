0ccca82439ae9d736a0f31b6cdc409b89c6c4cd4
GustavoSantos25 <gustavo.war2015@gmail.com>
2022-03-27 15:48:00 -0300

    Attacker strategy

--- src/attacker.c
+++ src/attacker.c
@@ -13,17 +13,60 @@
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
 /*----------------------------------------------------------------------------*/
+/*                              PRIVATE FUNCTIONS                              */
+/*----------------------------------------------------------------------------*/
+
+int check_way(int dir_ind, int number_of_directions, int cur_way){
+  int way = cur_way;
+  if(dir_ind == 0) way = 1;
+  else if(dir_ind == number_of_directions - 1) way = 0;
+  return way;
+}
+
+int change_direction_index(int prev_dir_ind, int cur_way){
+  int ind;
+  if(cur_way == 1) ind = prev_dir_ind + 1;
+  else return ind = prev_dir_ind - 1;
+  return ind;
+}
+
+
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
+// TODO: unused parameters, remove these lines later
   UNUSED(defender_spy);
+  
+  static position_t previous_position = INVALID_POSITION;
+  direction_t directions[5] = {DIR_DOWN, DIR_DOWN_RIGHT, DIR_RIGHT, DIR_UP_RIGHT, DIR_UP};
+  static direction_t best_direction;
+  static int best_direction_index = 1;
+  static int previous_direction_index;
+  static int current_way = 1;
+ 
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  if (equal_positions(attacker_position, previous_position)) {
+    // if(get_spy_number_uses(defender_spy) > 0){
+    //   position_t defender_position = get_spy_position(defender_spy);
+
+      
+    // }
+
+    current_way = check_way(previous_direction_index, 5, current_way);
+    best_direction_index = change_direction_index(previous_direction_index, current_way);
+    
+  }
+
+  best_direction = directions[best_direction_index];
+  
+  previous_direction_index = best_direction_index;
+  previous_position = attacker_position;
+  return best_direction;
 }
 
 /*----------------------------------------------------------------------------*/
